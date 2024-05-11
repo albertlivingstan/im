da1a81006ef094c8fc9083eb7803b92b07ea23e0
albertlivingstan <albertlivingstan@karunya.edu.in>
2024-05-11 20:03:37 +0530

    new img

--- greyImage .py
+++
@@ -1,4 +0,0 @@
-from PIL import Image
-myImage = Image.open('im/Blue_And_Red_Futuristic_Game_YouTube_Channel_Art_640x360_2.png') 
-greyImage = myImage.convert('L') #to save - greyImage.save('location') 
-greyImage.show()
--- to flip img.py
+++ to flip img.py
@@ -1,5 +1,5 @@
 from PIL import Image
-myImage = Image.open("im/Blue_And_Red_Futuristic_Game_YouTube_Channel_Art_640x360_2.png")
+myImage = Image.open("image-prossing/Blue_And_Red_Futuristic_Game_YouTube_Channel_Art_640x360_2.png")
 flippedImage = myImage.transpose(Image.FLIP_LEFT_RIGHT) 
 flippedImage.save('flip.png')
 myImage.show()
